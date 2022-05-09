4cffa0f40094288d305a895d9d924ef4417c0ad3
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2022-05-09 13:48:02 +0200

    nrn

--- IntroDataJpa/src/main/java/com/salesianostriana/dam/introdatajpa/model/Alumno.java
+++ IntroDataJpa/src/main/java/com/salesianostriana/dam/introdatajpa/model/Alumno.java
@@ -1,9 +1,19 @@
 package com.salesianostriana.dam.introdatajpa.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
 public class Alumno {
 	
