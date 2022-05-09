dc990a2ae16337a95ed3145d9d5495fe782ddc1c
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2022-05-09 14:26:39 +0200

    mven!

--- Clases-Proyecto/src/main/java/com/salesianostriana/dam/clasesproyecto/model/Categoria.java
+++ Clases-Proyecto/src/main/java/com/salesianostriana/dam/clasesproyecto/model/Categoria.java
@@ -2,8 +2,15 @@ package com.salesianostriana.dam.clasesproyecto.model;
 
 import java.util.List;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Categoria {
 	
 	private List <Producto> lista;
+	private String nombre;
+	@Id
+	@GeneratedValue
+	private long id;
 	
 }
