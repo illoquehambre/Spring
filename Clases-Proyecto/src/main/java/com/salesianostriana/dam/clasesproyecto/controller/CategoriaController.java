34a838af20309caa947673ee697907c02ae5d387
illoquehambre <illoquehambre@gmail.com>
2022-05-11 01:00:24 +0200

    fnb

--- Clases-Proyecto/src/main/java/com/salesianostriana/dam/clasesproyecto/controller/CategoriaController.java
+++ Clases-Proyecto/src/main/java/com/salesianostriana/dam/clasesproyecto/controller/CategoriaController.java
@@ -1,5 +1,15 @@
 package com.salesianostriana.dam.clasesproyecto.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class CategoriaController {
+	
+	@GetMapping("/listado")
+	public String listado() {
+		return "Carta2";
+	}
 
 }
